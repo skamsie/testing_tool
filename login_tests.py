e9c9db5e2148fb556e78f3b5e53079add0f2de32
skamsie <skamsie@yahoo.com>
2015-03-15 19:27:48 +0100

    .

--- login_tests.py
+++ login_tests.py
@@ -3,7 +3,6 @@ import unittest
 import login
 
 
-
 HTTP_ACCESS = '' #username:password@ OR leave empty if no http authentication
 DOMAIN = '' #domain name (not prefixed by 'http://'' or 'www')
 URL = 'http://%s%s' %(HTTP_ACCESS, DOMAIN)
@@ -11,27 +10,25 @@ USERNAME = '' #username / user_email
 PASSWORD = '' #password
 BROWSERS = {'firefox': webdriver.Firefox, 'chrome': webdriver.Chrome}
 
-class LoginTest(unittest.TestCase):
+
+class BlacklaneTest(unittest.TestCase):
   
   @classmethod
   def setUp(cls):
     cls.driver = BROWSERS['firefox']()
     cls.driver.maximize_window()
-    cls.user_login = login.UserLogin(cls.driver)
+    cls.usr_login = login.UserLogin(cls.driver)
+    cls.usr_login.driver.get(URL)
       
   def test_login_with_correct_credentials(self):
-    ul = self.user_login
-    ul.driver.get(URL)
-    ul.login_button.click()
-    ul.submit_credentials(USERNAME, PASSWORD)
-    self.assertTrue(ul.check_login_successful())
+    self.usr_login.login_button.click()
+    self.usr_login.submit_credentials(USERNAME, PASSWORD)
+    self.assertTrue(self.usr_login.check_login_successful())
 
   def test_login_with_incorrect_credentials(self):
-    ul = self.user_login
-    ul.driver.get(URL)
-    ul.login_button.click()
-    ul.submit_credentials('mxmx@yaho.com', 'PASSWORD')
-    self.assertTrue(ul.check_login_unsuccessful())
+    self.usr_login.login_button.click()
+    self.usr_login.submit_credentials('mxmx@yaho.com', 'PASSWORD')
+    self.assertTrue(self.usr_login.check_login_unsuccessful())
 
   @classmethod
   def tearDown(cls):
